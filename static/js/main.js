d26efeacd80874b29cd39ccf3f9e047fd00149f4
ppabba101 <pranav.pabba@gmail.com>
2024-01-27 23:02:29 -0800

    Tried getting orientation to work

--- static/js/main.js
+++ static/js/main.js
@@ -1,7 +1,12 @@
  /**
-         * Track the user location and continuously get latitude and longitude.
-         * @param {Object} onSuccess - Callback function to handle successful location tracking.
-         */
+ * Track the user location and continuously get latitude and longitude.
+ * @param {Object} onSuccess - Callback function to handle successful location tracking.
+ */
+
+//  window.addEventListener("deviceorientation", (event) => {
+//     console.log(`${event.alpha} : ${event.beta} : ${event.gamma}`);
+//   });
+  
  const continuousLocation = onSuccess => {
     if ('geolocation' in navigator === false) {
     // Handle lack of geolocation support
@@ -50,12 +55,11 @@ const instantLocation = onSuccess => {
     );
 };
 
-// Example usage:
 continuousLocation((lat, lng) => {
-    // Do something with the latitude and longitude values
     console.log(`Latitude: ${lat.toFixed(5)}, Longitude: ${lng.toFixed(5)}`);
 });
 
+// When tag button is clicked, get the current location and timestamp and convert to JSON
 document.getElementById('tag').addEventListener('click', function() {
     instantLocation((lat, lng) => {
         console.log('Tag button clicked, time: ' + new Date().toISOString());
@@ -68,3 +72,42 @@ document.getElementById('tag').addEventListener('click', function() {
         console.log(`Tag button -- ${jsonLocationData}`);
     });
 });
+
+
+// // // Compass stuffs
+function handleOrientation(event) {
+    console.log("Measuring direction...")
+    const compassDirection = event.alpha;
+    console.log('Compass Direction:', compassDirection);
+}
+
+// if (window.DeviceOrientationEvent) {
+//     console.log('DeviceOrientationEvent is supported')
+//     window.addEventListener('deviceorientation', handleOrientation);
+// } else {
+//     console.log('DeviceOrientationEvent is not supported');
+// }
+
+if (typeof window.webkitCompassHeading!== "undefined") {
+    alpha = window.webkitCompassHeading; //iOS non-standard
+    var heading = alpha;
+    console.log('Angle' + heading);
+  }
+else {
+    alert("Your device is reporting relative alpha values, so this compass won't point north! ");
+    alpha = window.webkitCompassHeading;
+    var heading = 360 - alpha; //heading [0, 360)
+    console.log('Angle' + heading);
+}
+
+if (window.DeviceOrientationAbsoluteEvent) {
+    console.log("DeviceOrientationAbsoluteEvent is supported")
+    window.addEventListener("DeviceOrientationAbsoluteEvent", handleOrientation);
+} // If not, check if the device sends any orientation data
+else if(window.DeviceOrientationEvent){
+    console.log("DeviceOrientationEvent is supported")
+    window.addEventListener("deviceorientation", handleOrientation);
+} // Send an alert if the device isn't compatible
+else {
+  alert("Sorry, try again on a compatible mobile device!");
+}
