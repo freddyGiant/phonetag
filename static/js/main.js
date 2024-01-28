2875ab3c4642cec992671a65094cd968d9743d96
freddyGiant[Laptop] <vincyfboling@gmail.com>
2024-01-27 21:23:52 -0800

    Add Socket support

--- static/js/main.js
+++ static/js/main.js
@@ -1,52 +1,60 @@
- /**
-         * Track the user location and continuously get latitude and longitude.
-         * @param {Object} onSuccess - Callback function to handle successful location tracking.
-         */
+/**
+ * TODO:
+ * - decide position cache size (time (10s)? number of positions?)
+ * - remember what i was gonna put here
+ */
+
+const socket = io();
+
+/**
+ * Track the user location and continuously get latitude and longitude.
+ * @param {Object} onSuccess - Callback function to handle successful location tracking.
+ */
  const continuousLocation = onSuccess => {
     if ('geolocation' in navigator === false) {
-    // Handle lack of geolocation support
-    console.error('Geolocation is not supported by your browser.');
-    return;
+        // Handle lack of geolocation support
+        console.error('Geolocation is not supported by your browser.');
+        return;
     }
 
     // Continuously watch the user's position and call the onSuccess callback with coordinates
     navigator.geolocation.watchPosition(
-    ({ coords: { latitude: lat, longitude: lng } }) => {
-        onSuccess(lat, lng);
-    },
-    error => {
-        // Handle location tracking errors
-        console.error('Error getting location /f continuous:', error.message || getPositionErrorMessage(error.code));
-    },
-    {
-        enableHighAccuracy: true,
-        timeout: 10000,
-        maximumAge: 0
-    }
+        ({ coords: { latitude: lat, longitude: lng } }) => {
+            onSuccess(lat, lng);
+        },
+        error => {
+            // Handle location tracking errors
+            console.error('Error getting location /f continuous:', error.message || getPositionErrorMessage(error.code));
+        },
+        {
+            enableHighAccuracy: true,
+            timeout: 10000,
+            maximumAge: 0
+        }
     );
 };
 
 const instantLocation = onSuccess => {
     if ('geolocation' in navigator === false) {
-    // Handle lack of geolocation support
-    console.error('Geolocation is not supported by your browser.');
-    return;
+        // Handle lack of geolocation support
+        console.error('Geolocation is not supported by your browser.');
+        return;
     }
 
     // Continuously watch the user's position and call the onSuccess callback with coordinates
     navigator.geolocation.getCurrentPosition(
-    ({ coords: { latitude: lat, longitude: lng } }) => {
-        onSuccess(lat, lng);
-    },
-    error => {
-        // Handle location tracking errors
-        console.error('Error getting location f/ instant:', error.message || getPositionErrorMessage(error.code));
-    },
-    {
-        enableHighAccuracy: true,
-        timeout: Infinity,
-        maximumAge: 0
-    }
+        ({ coords: { latitude: lat, longitude: lng } }) => {
+            onSuccess(lat, lng);
+        },
+        error => {
+            // Handle location tracking errors
+            console.error('Error getting location f/ instant:', error.message || getPositionErrorMessage(error.code));
+        },
+        {
+            enableHighAccuracy: true,
+            timeout: Infinity,
+            maximumAge: 0
+        }
     );
 };
 
