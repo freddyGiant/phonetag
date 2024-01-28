98fac4b27ccc03abb44dae4b6c0ce35c4ac03bf8
Vincent B <vincyfboling@gmail.com>
2024-01-28 00:35:52 -0800

    Feature/websocket (#3)
    * Add Socket support
    
    * Send and print location data over socket

--- app.py
+++ app.py
@@ -1,10 +1,16 @@
 from flask import Flask, render_template
+from flask_socketio import SocketIO
 
 app = Flask(__name__)
+socket = SocketIO(app)
 
 @app.route('/')
 def home():
     return render_template('index.html')
 
+@socket.on('tag')
+def handleTag(location_data):
+    print(location_data)
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    socket.run(app)
